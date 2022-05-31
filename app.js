f20afd3f5a75d595f465aeb5c032e0fb990a1a70
nyo <naimyasirozcan@gmail.com>
2022-05-31 21:37:01 +0300

    local storage added.

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 
+let counter = localStorage.getItem('counter') ? Number(localStorage.getItem('counter')) : 100
 let counterDOM = document.querySelector('#counter')
 let azalt = document.querySelector('#increase')
 let arttir = document.querySelector('#decrease')
-let counter = parseInt(counterDOM.innerHTML)
+
+counterDOM.innerHTML = counter
+
+
 arttir.addEventListener('click', clickEvent)
 azalt.addEventListener('click', clickEvent)
 
-function clickEvent(){
+function clickEvent() {
     this.id == 'increase' ? counter -= 1 : counter += 1
+    localStorage.setItem('counter', counter)
     counterDOM.innerHTML = counter
 }
+
+
+
+
+
